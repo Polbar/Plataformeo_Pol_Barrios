b1d4f32c58b53a58b37ea517728350c4e86c1b93
Polbar <polbarpolb@gmail.com>
2024-10-18 10:31:51 +0200

    star

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     private int coins = 0;
+    private int starsCollected = 0; // Contador de estrellas
 
     private bool isPaused;
 
+    [SerializeField] Text _coinText;
+    [SerializeField] GameObject _pauseCanvas;
+
+    // Array de referencias a los objetos de estrellas
+    [SerializeField] GameObject[] estrellasActivas; // Cambia a un array
+
+    private Animator _pausePanelAnimator;
+
+    private bool pauseAnimator;
+
     void Awake()
     {
-        if(instance != null && instance != this)
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
         }
@@ -20,26 +32,50 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+
+        _pausePanelAnimator = _pauseCanvas.GetComponentInChildren<Animator>();
     }
 
     public void Pause()
     {
-        if(!isPaused)
+        if (!isPaused)
         {
-            Time.timeScale = 0;
             isPaused = true;
+            Time.timeScale = 0;
+            _pauseCanvas.SetActive(true);
         }
-        else
+        else if (isPaused && !pauseAnimator)
         {
-            Time.timeScale = 1;
-            isPaused = false;
+            pauseAnimator = true;
+            StartCoroutine(ClosePauseAnimation());
         }
+    }
+
+    IEnumerator ClosePauseAnimation()
+    {
+        _pausePanelAnimator.SetBool("Close", true);
+        yield return new WaitForSecondsRealtime(0.50f);
+        Time.timeScale = 1;
+        _pauseCanvas.SetActive(false);
+        isPaused = false;
+        pauseAnimator = false;
     }
 
     public void AddCoin()
     {
         coins++;
+        _coinText.text = coins.ToString();
     }
 
+    public void AddStar()
+    {
+        // Incrementar el contador de estrellas recogidas
+        starsCollected++;
 
+        // Activar la estrella correspondiente si existe
+        if (starsCollected - 1 < estrellasActivas.Length)
+        {
+            estrellasActivas[starsCollected - 1].SetActive(true);
+        }
+    }
 }
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private bool interactable;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && interactable)
+        {
+            GameManager.instance.AddStar();
+            SoundManager.instance.PlaySFX(SoundManager.instance._audioSource, SoundManager.instance.coinAudio);
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            interactable = false;
+        }
+    }
+}
